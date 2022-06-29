fb8d35c02398fe22201f5fafbff587a1752c40d6
Dan Grossberg <dwgrossberg@gmail.com>
2022-06-29 21:58:02 +0700

    Add first Gameboard tests and moduel properties

--- src/__tests__/gameboard.test.js
+++ src/__tests__/gameboard.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import Gameboard from "../scripts/factories/gameboard";
+import Ship from "../scripts/factories/ship";
 
 describe("Gameboard Factory", () => {
   describe("type", () => {
@@ -13,12 +14,17 @@ describe("Gameboard Factory", () => {
 
   describe("basic Gameboard properties", () => {
     const board1 = Gameboard("test");
+    const carrier = Ship("carrier", 5);
     test("Gameboard data object logs player namer correctly", () => {
       expect(board1.data.player).toBe("test");
     });
     test("Gameboard is 100 spaces long", () => {
       board1.initBoard();
       expect(board1.data.board.length).toBe(100);
+    });
+    test("Gameboard can place horizontal ships at specific coords", () => {
+      board1.placeShip(carrier, 33);
+      expect(board1.data.board[33].hasShip).toBe(true);
     });
   });
 });
--- src/scripts/factories/ship.js
+++ src/scripts/factories/ship.js
@@ -2,6 +2,7 @@ const Ship = (name, length) => {
   return {
     name: name,
     length: length,
+    vertical: false,
     hits: Array(length).fill(null),
     hit(num) {
       this.hits[num] = "hit";
