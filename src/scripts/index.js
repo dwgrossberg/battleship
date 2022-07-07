2cc1c26308c94ac2297d7694581ac35b1d97bf10
Dan Grossberg <dwgrossberg@gmail.com>
2022-07-07 21:47:56 +0700

    Add scss styles for footer and header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
 import favicon from "../assets/favicon.ico";
+import githubIcon from "../assets/gitHubIconWhite.png";
 import "../styles/index.scss";
 import gameController from "./modules/gameControl.js";
 
 document.querySelector('[type="image/x-icon"]').href = favicon;
-
-console.log(favicon);
+document.getElementById("github-icon").src = githubIcon;
 
 gameController.startGame("player 1");
